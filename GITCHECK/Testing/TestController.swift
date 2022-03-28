f88de20c26841a4eab74877fb22b4c45da1ec86e
Ayyarappan <ayyarappan@pepul.com>
2022-03-28 13:52:13 +0530

    Added testing folder

---
+++ GITCHECK/Testing/TestController.swift
@@ -0,0 +1,18 @@
+//
+//  TestController.swift
+//  GITCHECK
+//
+//  Created by Ayyarappan K on 28/03/22.
+//
+
+import UIKit
+
+class TestController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
